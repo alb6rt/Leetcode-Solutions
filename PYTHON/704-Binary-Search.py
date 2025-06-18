c33ae25bed4772b54fa0c890d318f66bc1a8e6f0
Albert <94102356+alb6rt@users.noreply.github.com>
2025-06-19 00:29:34 +1000

    Update 704-Binary-Search.py

--- PYTHON/704-Binary-Search.py
+++ PYTHON/704-Binary-Search.py
@@ -1,21 +1,16 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums) - 1
 
-        while l <= r:
+        while (l <= r):
             m = (l + r) // 2
-            if target > nums[m]:
-                l = m + 1
-            elif target < nums[m]:
+
+            if (target < nums[m]):
                 r = m - 1
+            elif (target > nums[m]):
+                l = m + 1
             else:
                 return m
         
         return -1
-        
